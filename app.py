6e9858647ba8938e711433db6f2efd9d2472cb88
Thiennn262626 <nguyendinhthien10a5@gmail.com>
2024-03-25 01:20:11 +0700

    connect sql server

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
-from flask import Flask
+import os
 
-# from api.views import blueprint
+from flask import Flask
+from dotenv import load_dotenv
+
+from api.views import blueprint
 
 app = Flask(__name__)
 app.config.from_object('config')
-# app.register_blueprint(blueprint=blueprint)
+app.register_blueprint(blueprint=blueprint)
 
 host = app.config.get("APP_HOST", "0.0.0.0")
 port = app.config.get("FLASK_DEVELOPMENT_PORT", 8000)
@@ -18,5 +21,10 @@ def hello():
 def helloo():
     return port
 
+@app.route('/b', methods=['GET'])
+def hellooo():
+    return os.getenv('FLASK_DEBUG')
+
 if __name__ == '__main__':
+    load_dotenv()
     app.run(host=host, port=port, debug=debug)
--- connection_hander.py
+++ connection_hander.py
@@ -10,7 +10,8 @@ load_dotenv()
 
 class ConnectionHandler:
     def __init__(self):
-        connection_string = os.environ.get("SQL_SERVER_STRING")
+        # connection_string = os.environ.get("SQL_SERVER_STRING")
+        connection_string = os.getenv("SQL_SERVER_STRING")
         self.conn = pyodbc.connect(connection_string)
 
     def fetch_data(self, query):
