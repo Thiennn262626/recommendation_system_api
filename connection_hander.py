b37279e0fc4f2e0d5bbf5ec9a9088af567084d2e
Thiennn262626 <nguyendinhthien10a5@gmail.com>
2024-03-25 00:46:56 +0700

    connect sql server

---
+++ connection_hander.py
@@ -0,0 +1,35 @@
+# from datetime import datetime
+# from sqlalchemy.engine import URL
+# from sqlalchemy import create_engine
+# from sqlalchemy.pool import QueuePool
+# import pandas as pd
+import os
+from dotenv import load_dotenv
+import pyodbc
+load_dotenv()
+
+class ConnectionHandler:
+    def __init__(self):
+        connection_string = os.environ.get("SQL_SERVER_STRING")
+        self.conn = pyodbc.connect(connection_string)
+
+    def fetch_data(self, query):
+        cursor = self.conn.cursor()
+        cursor.execute(query)
+        rows = cursor.fetchall()
+        id_list = [row[0] for row in rows]
+        return id_list
+
+#     def insert_data(self, df, tablename):
+#         df.to_sql(tablename, if_exists='append', index=False, con=self.db_connection)
+#
+#     def execute_query(self, query):
+#         self.db_connection.execute(query)
+#
+#     def __del__(self):
+#         try:
+#             self.db_connection.close()
+#         except:
+#             None
+
+db_connection = ConnectionHandler()
