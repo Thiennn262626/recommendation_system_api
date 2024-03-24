2f2a302afadc9ed64f358711d0c1d5bcd2b66eae
Thiennn262626 <nguyendinhthien10a5@gmail.com>
2024-03-25 00:44:40 +0700

    connect sql server

--- api/views.py
+++ api/views.py
@@ -1,13 +1,12 @@
 from flask import Blueprint
 from flask_restful import Api
 
-from api.resources.user import UserList, UserResource
+from api.resources.user import UserList
 
 blueprint = Blueprint('api', __name__, url_prefix='/api')
 api = Api(blueprint, errors=blueprint.errorhandler)
 
 api.add_resource(UserList, "/users")
-api.add_resource(UserResource, "/users/<int:user_id>")
 
 
 
--- users.py
+++
@@ -1,6 +0,0 @@
-users = [
-    {"id": 0, "name": "Thien", "email": "thien@gmail.com", "age": 23},
-    {"id": 1, "name": "Thien2", "email": "thien2@gmail.com", "age": 24},
-    {"id": 2, "name": "Thien3", "email": "thien3@gmail.com", "age": 25},
-    {"id": 3, "name": "Thien4", "email": "thien4@gmail.com", "age": 26}
-]
