68d15593a8e246535448337800630561a5599d43
Thiennn262626 <nguyendinhthien10a5@gmail.com>
2024-05-06 16:37:45 +0700

    string

--- api/dbs/connection_hander.py
+++ api/dbs/connection_hander.py
@@ -9,6 +9,7 @@ load_dotenv()
 class ConnectionHandler:
     def __init__(self):
         connection_string = os.getenv("SQL_SERVER_STRING")
+        print("connection_string: ", connection_string)
         self.conn = pyodbc.connect(connection_string)
         print("ConnectionHandler init.", self.conn)
 
